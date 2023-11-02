02245583dd572796eb547cb5885628eec5319a0a
0xsuk <iam0xsuk@gmail.com>
2023-11-02 14:03:40 +0900

    crate-digger

---
+++ crate-digger/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+import org.deepsymmetry.cratedigger.FileFetcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        FileFetcher fetcher = FileFetcher.getInstance();
+        System.out.println( "Hello World!" );
+    }
+}
