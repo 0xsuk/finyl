d9e44f973895ac561798aa6cd0f6420c517657ac
0xsuk <iam0xsuk@gmail.com>
2023-11-25 19:12:19 +0900

    digging it

--- crate-digger/src/main/java/com/example/App.java
+++ crate-digger/src/main/java/com/example/App.java
@@ -1,23 +1,29 @@
 package com.example;
 import org.deepsymmetry.cratedigger.Database;
+import org.deepsymmetry.cratedigger.pdb.*;
 import java.io.File;
 import java.io.IOException;
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        File pdb = new File("./src/main/java/com/example/export.pdb");
-        System.out.println(pdb);
+        File pdb = new File("./export.pdb");
         try {
             Database database = new Database(pdb);
-            System.out.println("success");
-        
+            Map<Long, RekordboxPdb.TrackRow> map = database.trackIndex;
+            for (Map.Entry<Long, RekordboxPdb.TrackRow> entry : map.entrySet()) {
+                Long key = entry.getKey();
+                RekordboxPdb.TrackRow row = entry.getValue();
+                System.out.println("key:" + key);
+                //Only use database
+                System.out.println("filename:" + Database.getText(row.filePath()));
+                System.out.println(row.tempo());
+            }
+            System.out.println("size is " + map.size());
         } catch (IOException e) {
-            //
             System.out.println(e);
         }
     }
