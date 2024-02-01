528bc0a38614be1d55920945bdf7bc67638b9bb7
null <iam0xsuk@gmail.com>
2024-02-01 20:47:02 +0900

    kaitai test

--- test-kaitai.cpp
+++ test-kaitai.cpp
@@ -10,10 +10,28 @@ int main() {
   kaitai::kstream ks(&ifs);
 
   rekordbox_pdb_t rekordboxDB = rekordbox_pdb_t(&ks);
+  constexpr int totalTables = 8;
+  rekordbox_pdb_t::page_type_t tableOrder[totalTables] = {
+    rekordbox_pdb_t::PAGE_TYPE_KEYS,
+    rekordbox_pdb_t::PAGE_TYPE_GENRES,
+    rekordbox_pdb_t::PAGE_TYPE_ARTISTS,
+    rekordbox_pdb_t::PAGE_TYPE_ALBUMS,
+    rekordbox_pdb_t::PAGE_TYPE_PLAYLIST_ENTRIES,
+    rekordbox_pdb_t::PAGE_TYPE_TRACKS,
+    rekordbox_pdb_t::PAGE_TYPE_PLAYLIST_TREE,
+    rekordbox_pdb_t::PAGE_TYPE_HISTORY};
   
-  for (std::vector<rekordbox_pdb_t::table_t*>::iterator table = rekordboxDB.tables()->begin();
-       table != rekordboxDB.tables()->end();
-       ++table) {
-    printf("enum : %d\n", (*table)->type());
+  for (int tableOrderIndex = 0; tableOrderIndex < totalTables; tableOrderIndex++) {
+    for (std::vector<rekordbox_pdb_t::table_t*>::iterator table = rekordboxDB.tables()->begin();
+         table != rekordboxDB.tables()->end();
+         ++table) {
+      printf("enum : %d\n", (*table)->type());
+      if ((*table)->type() != tableOrder[tableOrderIndex]) {
+        continue;
+      }
+
+      uint16_t lastIndex = (*table)->last_page()->index();
+      rekordbox_pdb_t::page_ref_t* currentRef = (*table)->first_page();
+    }
   }
 }
